688dcb1d856ffb9ccd81e2238f663f95bf791a31
sandalisingh <sandalisingh.02@gmail.com>
2024-02-24 21:27:19 +0530

    update

--- scripts/ActionGeneration.py
+++ scripts/ActionGeneration.py
@@ -4,18 +4,19 @@ class ActionGenerator:
 
     def __init__(self):
         # Initialize Q-table
-        no_of_personality_states = 5    # OCEAN personality model
-        no_of_ranges_of_personality_states = 3  # 3 ranges for each personality states (0-3, 4-7, 8-10)
-        no_of_emotional_states = 10  # 10 emotional states
-        no_of_action_states = 10  # 10 action states
+        self.no_of_personality_states = 5    # OCEAN personality model
+        self.no_of_ranges_of_personality_states = 3  # 3 ranges for each personality states (0-3, 4-7, 8-10)
+        self.no_of_emotional_states = 10  # 10 emotional states
+        self.no_of_action_states = 10  # 10 action states
 
         # initialize Q table
         # 5 ocean personality * 3 ranges (0-3,4-7,8-10) * 10 emotional states * 10 FROM action states * 10 TO action states
-        Q = np.random.random((no_of_personality_states, no_of_ranges_of_personality_states, no_of_emotional_states, no_of_action_states, no_of_action_states))  # Initialize Q-table with zeros
+        self.Q = np.random.random((no_of_personality_states, no_of_ranges_of_personality_states, no_of_emotional_states, no_of_action_states, no_of_action_states))  # Initialize Q-table with zeros
 
         # Parameters
-        alpha = 0.1  # Learning rate
-        gamma = 0.9  # Discount factor
+        self.alpha = 0.1  # Learning rate
+        self.gamma = 0.9  # Discount factor
+        self.epsilon = 0.1  # Exploration rate
 
     # Define reward function (example)
     def calculate_reward(personality_vector, current_action, next_action):
@@ -36,27 +37,39 @@ class ActionGenerator:
             # Add more conditions for other emotional states and actions
             
         # Add rewards for other personality types
+
+        reward /= 5
+        print("Reward = ", reward)
         
         return reward
 
-    def action_generator(personality_vector, current_state) :
+    def action_generator(self, personality_vector, emotional_state_index, previous_action_state_index) :
         # Choose action based on epsilon-greedy policy
-        epsilon = 0.1  # Exploration rate
-        if np.random.rand() < epsilon:
-            final_action_state_index = np.random.randint(no_of_action_states)  # Choose random action
+        if np.random.rand() < epsilon: 
+            final_action_state_index = np.random.randint(self.no_of_action_states)  # Choose random action
         else:
-            final_action_state_index = np.argmax(Q[current_state])  # PROBABILITY MULTIPLICATION !!!!
+            # initialize an array for 10 action states with value 1
+            q_val_array = np.ones(10)
+
+            for i in (0,4):     # 5 ocean personalities
+                for j in (0,9) :    # 10 action states
+                    # Q value of jTH action state = product of Q value of the jTH action state of each ocean personality
+                    q_val_array[j] *= Q[i][(personality_vector[i]/3)-1][emotional_state_index][previous_action_state_index][j]
+            
+            # select action state with max q value
+            final_action_state_index = np.argmax(q_val_array)  # gives the index of that action state
+            print("Final action state index = ", final_action_state_index)
 
         return final_action_state_index
 
-    def q_learning(personality_vector, current_state, next_state) :
+    def q_learning(self, personality_vector, current_state, next_state) :
         # Define current and next states
         # current_state = (emotional_state_index, action_state_index)
         # next_state = (next_emotional_state_index, next_action_state_index)
 
         # Execute action and observe reward         ### DIALOGUE GENERATION !!!!!
-        reward = calculate_reward(current_state[1], next_state[1])
+        reward = self.calculate_reward(current_state[1], next_state[1])
 
         # Update Q-value using Q-learning update rule
         # for dominant personality !!!!
-        Q[current_state][final_action_state_index] = (1 - alpha) * Q[current_state][final_action_state_index] + alpha * (reward + gamma * np.max(Q[next_state]))
+        self.Q[current_state][final_action_state_index] = (1 - alpha) * self.Q[current_state][final_action_state_index] + alpha * (reward + gamma * np.max(self.Q[next_state]))
