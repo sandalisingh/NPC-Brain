48d4521ffad3cca824fb7300142a9e75aa5cfb30
Tanishka Anand <tanu182003@gmail.com>
2024-02-25 00:57:06 +0530

    frontend dal gaya

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Range from './components/Range';
-import Environment from './components/Environment';
-import Slider from './components/Slider';
-import emotions from './components/emotions'
 import SliderComparison from './components/SliderComparison';
-import StopWords from './components/StopWords';
 import EmotionTagger from './components/EmotionTagger';
 // import Lemmatizer from './components/Lemmatizer';
 
