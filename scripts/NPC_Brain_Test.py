a066d4ea50d658c524beee7131e9ca601a7b84cd
sandalisingh <sandalisingh.02@gmail.com>
2024-03-14 16:16:53 +0530

    Emojis updated

--- scripts/NPC_Brain_Test.py
+++ scripts/NPC_Brain_Test.py
@@ -6,16 +6,20 @@ environemnt = input("Environment = ")
 
 NPC = NPC_Brain(personality_vector, environemnt)
 
-emotion = NPC.get_emoji()
+emoji = NPC.get_emoji()
+emotion = NPC.get_current_emotion_state()
 action = NPC.get_current_action_state()
+print("BOT - emoji = ", emoji)
 print("BOT - emotion = ", emotion)
 print("BOT - action = ", action)
 
 while(True):
     chat = input("Chat = ")
     reply = NPC.generate_reply(chat)
-    emotion = NPC.get_emoji()
+    emoji = NPC.get_emoji()
+    emotion = NPC.get_current_emotion_state()
     action = NPC.get_current_action_state()
     print("BOT - response = ", reply)
+    print("BOT - emoji = ", emoji)
     print("BOT - emotion = ", emotion)
     print("BOT - action = ", action)
