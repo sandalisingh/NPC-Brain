b8c688bbaa2a7132fb4e74747abd0fc1df7db762
sandalisingh <sandalisingh.02@gmail.com>
2024-05-03 10:56:24 +0530

    Update

--- backend/DataVisualizer.py
+++ backend/DataVisualizer.py
@@ -1,7 +1,6 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
-import graphviz
 from States import logging
 from prettytable import PrettyTable
 import tensorflow as tf
